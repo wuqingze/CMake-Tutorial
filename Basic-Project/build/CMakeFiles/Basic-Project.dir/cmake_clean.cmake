680f5cb271dbf6058bd6610c57a0d9b001e6d1e2
wuqingze <nongxiaolang@foxmail.com>
2021-01-10 01:39:26 +0800

    Basic CMake project

---
+++ Basic-Project/build/CMakeFiles/Basic-Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Basic-Project"
+  "Basic-Project.pdb"
+  "CMakeFiles/Basic-Project.dir/main.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Basic-Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
